e12af2c6b08162ad95edfcc1e55b6658a9219e8f
Mc Punyawat <6210612799@student.tu.ac.th>
2022-01-30 23:31:52 +0700

    Initial commit

---
+++ app/src/main/java/punyawat/randomnumberapp/MainActivity.kt
@@ -0,0 +1,78 @@
+package punyawat.randomnumberapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import kotlin.random.Random.Default.nextInt
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+    lateinit var editText: EditText
+    lateinit var resetbut: ImageButton
+    lateinit var okbut: ImageButton
+    lateinit var count: TextView
+
+
+    var random: Int = nextInt( 1,1000)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textView)
+        editText = findViewById(R.id.editText)
+        resetbut = findViewById(R.id.resetbut)
+        okbut = findViewById(R.id.okbut)
+        count = findViewById(R.id.count)
+        textView.text = "GUESS THE NUMBER 1-1000"
+
+        var x:Int = 0
+        okbut.setOnClickListener {
+            var number:Int = 0
+
+            x++
+            try {
+                number = editText.text.toString().toInt()
+
+            }
+            catch (e: NumberFormatException){
+
+            }
+            if ((number > 1000) or (number <=  0)){
+                textView.text = "pls put number between 1-1000"
+                editText.text.clear()
+            }
+                else if (number < random) {
+
+                textView.text = "HINTS : Number too low"
+
+                count.text=("count : $x")
+                editText.text.clear()
+            } else if (number > random) {
+                textView.text = "HINTS : Number too High"
+                count.text=("count : $x")
+                editText.text.clear()
+            } else {
+                textView.text = "YEAHHH U RIGHT!"
+                count.text=("count : $x")
+                editText.text.clear()
+
+            }
+        }
+
+        resetbut.setOnClickListener {
+            var x:Int = 0
+            count.text=("count : $x")
+            reset()
+
+        }
+    }
+    fun reset() {
+        random = nextInt(1,1000)
+        textView.text = "GUESS THE NUMBER 1-1000 (new number)"
+        editText.text.clear()
+    }
+}
